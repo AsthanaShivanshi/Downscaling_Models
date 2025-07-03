353b4e66fb76addd4a6d916990d6975084963f92
sasthana <shivanshi.asthana@unil.ch>
2025-07-03 17:36:56 +0200

    Changes

--- models_UNet/UNet_Deterministic_Training_Dataset/run_inference.sh
+++ models_UNet/UNet_Deterministic_Training_Dataset/run_inference.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=UNet_inference_training_dataset
-#SBATCH --output=logs/FULL_training/UNet_inference_%j.out
-#SBATCH --error=logs/FULL_training/UNet_inference_%j.err
+#SBATCH --output=logs/training/UNet_inference_%j.out
+#SBATCH --error=logs/training/UNet_inference_%j.err
 #SBATCH --time=3-00:00:00
 #SBATCH --partition=cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 
-mkdir -p logs/FULL_training
+mkdir -p logs/training
 module load python
 source environment.sh
 
@@ -16,4 +16,4 @@ cd models_UNet/UNet_Deterministic_Training_Dataset
 
 python Inference.py
 
-echo "Inference has finished, test loss printed"
+echo "Inference done, test loss printed"
