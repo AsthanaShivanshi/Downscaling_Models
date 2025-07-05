d3af02a312fbe2c564bad4cff296f96483abbf10
sasthana <shivanshi.asthana@unil.ch>
2025-07-05 17:31:24 +0200

    Changes

---
+++ models_UNet/UNet_Deterministic_Combined_Dataset/UNet_Experiments.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=UNet_Deterministic_Pretraining_Dataset
+#SBATCH --output=logs/pretraining/job_output-%j.txt
+#SBATCH --error=logs/pretraining/job_error-%j.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=500G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+set -x
+mkdir -p logs
+
+echo "Loading python module"
+module load python
+
+echo "Sourcing environment"
+source environment.sh
+echo "Environment sourced."
+
+cd models_UNet/UNet_Deterministic_Pretraining_Dataset
+
+export WANDB_MODE="online"
+export PYTHONUNBUFFERED=1
+which python
+python --version
+echo "Starting Main.py..."
+#python Main.py --quick_test
+
+#For full training, remove the --quick_test flag
+python Main.py
+echo "Main.py finished."
