579668b9d79ce66278e8536f7c799631f2c6eb40
sasthana <shivanshi.asthana@unil.ch>
2025-07-14 10:59:01 +0200

    Changes

--- models_UNet/UNet_Deterministic_Training_Dataset/Train.py
+++ models_UNet/UNet_Deterministic_Training_Dataset/Train.py
@@ -165,6 +165,7 @@ def train_model(model, train_loader, val_loader, optimizer, criterion, scheduler
         # Saving best model and inference weights/config
         if val_loss < best_val_loss:
             best_val_loss = val_loss
+            best_val_loss_per_channel=val_per_channel
             epochs_no_improve = 0  # Reset counter
             checkpoint_save(model, optimizer, epoch+1, val_loss, checkpoint_path, inference_path)
             save_model_config(config, model_config_path)
@@ -192,4 +193,4 @@ def train_model(model, train_loader, val_loader, optimizer, criterion, scheduler
             break
     print(f"best_val_loss: {best_val_loss}")
 
-    return model, history, best_val_loss
+    return model, history, best_val_loss,best_val_loss_per_channel
