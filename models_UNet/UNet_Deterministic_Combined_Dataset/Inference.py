8c28a1d70a868d044e84fd7ba281728006940df7
sasthana <shivanshi.asthana@unil.ch>
2025-07-16 17:53:22 +0200

    Changes

---
+++ models_UNet/UNet_Deterministic_Combined_Dataset/Inference.py
@@ -0,0 +1,101 @@
+import os
+import yaml
+import torch
+import xarray as xr
+import numpy as np
+import json
+import sys
+from UNet import UNet
+from Downscaling_Dataset_Prep import DownscalingDataset
+from torch.utils.data import DataLoader
+
+def descale_precip(x, min_val, max_val):
+    return x * (max_val - min_val) + min_val
+
+def descale_temp(x, mean, std):
+    return x * std + mean
+
+os.environ["BASE_DIR"] = "/work/FAC/FGSE/IDYST/tbeucler/downscaling"
+BASE_DIR = os.environ["BASE_DIR"]
+
+sys.path.append(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/models_UNet/UNet_Deterministic_Combined_Dataset"))
+
+# Load model
+model_path = os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/models_UNet/UNet_Deterministic_Combined_Dataset/combined_model_huber.pth")
+training_checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
+model_instance = UNet(in_channels=5, out_channels=4)
+model_instance.load_state_dict(training_checkpoint["model_state_dict"])
+model_instance.eval()
+
+# Load input and target datasets
+precip_input = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_precip_input_test_chronological_scaled.nc"))
+temp_input   = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_temp_input_test_chronological_scaled.nc"))
+tmin_input   = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_tmin_input_test_chronological_scaled.nc"))
+tmax_input   = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_tmax_input_test_chronological_scaled.nc"))
+
+precip_target = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_precip_target_test_chronological_scaled.nc"))
+temp_target   = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_temp_target_test_chronological_scaled.nc"))
+tmin_target   = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_tmin_target_test_chronological_scaled.nc"))
+tmax_target   = xr.open_dataset(os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset/combined_tmax_target_test_chronological_scaled.nc"))
+
+config_path = os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/models_UNet/UNet_Deterministic_Combined_Dataset/config.yaml")
+with open(config_path, 'r') as f:
+    config = yaml.safe_load(f)
+
+elevation_path = os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/elevation.tif")
+
+# Merge datasets for dataloader
+inputs_merged = xr.merge([precip_input, temp_input, tmin_input, tmax_input])
+targets_merged = xr.merge([precip_target, temp_target, tmin_target, tmax_target])
+
+ds = DownscalingDataset(inputs_merged, targets_merged, config, elevation_path)
+paired_ds = DataLoader(ds, batch_size=1, shuffle=False, num_workers=4)
+
+var_names = ["precip", "temp", "tmin", "tmax"]
+all_preds = []
+
+with torch.no_grad():
+    for input_batch, _ in paired_ds:
+        output_batch = model_instance(input_batch)
+        all_preds.append(output_batch.squeeze(0).cpu().numpy())
+
+all_preds = np.stack(all_preds)
+
+# Load scaling params
+scaling_dir = os.path.join(BASE_DIR, "sasthana/Downscaling/Downscaling_Models/Combined_Chronological_Dataset")
+rhiresd_params = json.load(open(os.path.join(scaling_dir, "combined_precip_scaling_params_chronological.json")))
+tabsd_params   = json.load(open(os.path.join(scaling_dir, "combined_temp_scaling_params_chronological.json")))
+tmind_params   = json.load(open(os.path.join(scaling_dir, "combined_tmin_scaling_params_chronological.json")))
+tmaxd_params   = json.load(open(os.path.join(scaling_dir, "combined_tmax_scaling_params_chronological.json")))
+
+# Denormalize predictions
+all_preds_denorm = np.empty_like(all_preds)
+all_preds_denorm[:, 0, :, :] = descale_precip(all_preds[:, 0, :, :], rhiresd_params["min"], rhiresd_params["max"])
+all_preds_denorm[:, 1, :, :] = descale_temp(all_preds[:, 1, :, :], tabsd_params["mean"], tabsd_params["std"])
+all_preds_denorm[:, 2, :, :] = descale_temp(all_preds[:, 2, :, :], tmind_params["mean"], tmind_params["std"])
+all_preds_denorm[:, 3, :, :] = descale_temp(all_preds[:, 3, :, :], tmaxd_params["mean"], tmaxd_params["std"])
+
+# Get 1D lat/lon
+if inputs_merged.lat.ndim == 2:
+    lat_1d = inputs_merged.lat.values[:, 0]
+    lon_1d = inputs_merged.lon.values[0, :]
+else:
+    lat_1d = inputs_merged.lat.values
+    lon_1d = inputs_merged.lon.values
+
+# Save predictions as xarray Dataset
+pred_vars = {}
+for i, var in enumerate(var_names):
+    pred_vars[var] = xr.DataArray(
+        all_preds_denorm[:, i, :, :],
+        dims=("time", "lat", "lon"),
+        coords={
+            "time": inputs_merged.time.values,
+            "lat": lat_1d,
+            "lon": lon_1d,
+        },
+        name=var
+    )
+
+pred_ds = xr.Dataset(pred_vars)
+pred_ds.to_netcdf("Combined_Downscaled_Predictions_2011_2020.nc")
