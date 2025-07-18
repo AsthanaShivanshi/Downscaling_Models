3db6af481605c245152ed10d2f20e45a2f93c0bd
sasthana <shivanshi.asthana@unil.ch>
2025-07-18 11:21:58 +0200

    Changes

--- BC_methods/BC_slurm.sh
+++ BC_methods/BC_slurm.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=logs/bc/job_output-%j.txt
 #SBATCH --error=logs/bc/job_error-%j.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --time=3-00:00:00
-#SBATCH --mem=128G
+#SBATCH --mem=256G
 #SBATCH --partition=cpu
 
 module load python
