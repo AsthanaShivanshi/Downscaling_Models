b320fd8980fb13a74bfa707a8748a09fb7f92ba5
sasthana <shivanshi.asthana@unil.ch>
2025-07-18 14:10:40 +0200

    Changes

--- BC_methods/BC_slurm.sh
+++ BC_methods/BC_slurm.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=logs/bc/job_output-%j.txt
 #SBATCH --error=logs/bc/job_error-%j.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --time=3-00:00:00
-#SBATCH --mem=500G
+#SBATCH --mem=256G
 #SBATCH --partition=cpu
 
 module load python
@@ -18,5 +18,5 @@ export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
 cd BC_methods
 
 echo "Starting EQM"
-python EQM_manual.py --n_jobs $SLURM_CPUS_PER_TASK
+python EQM.py --n_jobs $SLURM_CPUS_PER_TASK
 echo "EQM finished"
