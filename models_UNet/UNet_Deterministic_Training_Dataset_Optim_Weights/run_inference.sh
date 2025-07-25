ffb95b75c7a28a40c3079ab93eed33ff32edfb48
sasthana <shivanshi.asthana@unil.ch>
2025-07-25 12:31:44 +0200

    Inference for model runs

--- models_UNet/UNet_Deterministic_Training_Dataset_Optim_Weights/run_inference.sh
+++ models_UNet/UNet_Deterministic_Training_Dataset_Optim_Weights/run_inference.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=UNet_inference_combined_dataset
+#SBATCH --job-name=Model_Downscaling
 #SBATCH --output=logs/combined/UNet_inference_%j.out
 #SBATCH --error=logs/combined/UNet_inference_%j.err
 #SBATCH --time=08:00:00
 #SBATCH --partition=cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --mem=256G
 
 mkdir -p logs/training
 module load python
 source environment.sh
 cd models_UNet/UNet_Deterministic_Training_Dataset_Optim_Weights
 
-python Inference.py
+python Inference_Model.py
 
 echo "Inference done, test loss printed"
