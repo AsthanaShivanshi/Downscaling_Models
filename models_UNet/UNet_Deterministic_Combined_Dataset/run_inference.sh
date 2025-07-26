ec919637b4156b62ff75ecef8c704e0fad764158
sasthana <shivanshi.asthana@unil.ch>
2025-07-26 23:35:14 +0200

    Changes

--- models_UNet/UNet_Deterministic_Combined_Dataset/run_inference.sh
+++ models_UNet/UNet_Deterministic_Combined_Dataset/run_inference.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=UNet_inference_combined_dataset
-#SBATCH --output=logs/combined/UNet_inference_%j.out
-#SBATCH --error=logs/combined/UNet_inference_%j.err
-#SBATCH --time=3-00:00:00
+#SBATCH --job-name=Combined_inference_on_model
+#SBATCH --output=logs/model_downscaling_logs/combined_model_inference_%j.out
+#SBATCH --error=logs/model_downscaling_logs/combined_model_inference_%j.err
+#SBATCH --time=16:00:00
 #SBATCH --partition=cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --mem=256G
 
-mkdir -p logs/training
+mkdir -p logs/model_downscaling_logs
 module load python
 source environment.sh
 
 cd models_UNet/UNet_Deterministic_Combined_Dataset
 
-python Inference.py
+python Inference_Model.py
 
 echo "Inference done, test loss printed"
