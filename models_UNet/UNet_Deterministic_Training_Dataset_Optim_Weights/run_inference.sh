39211b4bc62f8642614d4d90ea017ed80ad138d0
sasthana <shivanshi.asthana@unil.ch>
2025-07-26 12:54:36 +0200

    Changes

--- models_UNet/UNet_Deterministic_Training_Dataset_Optim_Weights/run_inference.sh
+++ models_UNet/UNet_Deterministic_Training_Dataset_Optim_Weights/run_inference.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=Model_Downscaling
-#SBATCH --output=logs/combined/UNet_inference_%j.out
-#SBATCH --error=logs/combined/UNet_inference_%j.err
+#SBATCH --job-name=Single_timestep_test
+#SBATCH --output=logs/model_downscaling_logs/single_timestep_%j.out
+#SBATCH --error=logs/model_downscaling_logs/single_timestep_%j.err
 #SBATCH --time=08:00:00
 #SBATCH --partition=cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=480G
+#SBATCH --mem=64G
 
-mkdir -p logs/training
+mkdir -p logs/model_downscaling_logs
 module load python
 source environment.sh
 cd models_UNet/UNet_Deterministic_Training_Dataset_Optim_Weights
 
-python Inference_Model.py
+python Inference_Model_Timestep.py
 
 echo "Inference done, test loss printed"
