f5415b04ef1534ea84c9c32538158de05c7a0245
sasthana <shivanshi.asthana@unil.ch>
2025-07-31 12:51:59 +0200

    Changes

--- BC_methods/EQM_single_cell_Zürich.py
+++ BC_methods/EQM_single_cell_Zürich.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 from SBCK import QM
 import config
 
-model_path = f"{config.SCRATCH_DIR}/tmax_r01_HR_masked.nc"
-obs_path = f"{config.SCRATCH_DIR}/TmaxD_1971_2023.nc"
-output_path = f"{config.BC_DIR}/qm_tmax_r01_singlecell_output.nc"
-plot_path = f"{config.OUTPUTS_MODELS_DIR}/qm_correction_function_tmax_r01_zürich.png"
-cdf_plot_path = f"{config.OUTPUTS_MODELS_DIR}/qm_cdf_tmax_r01_zürich.png"
-map_plot_path = f"{config.OUTPUTS_MODELS_DIR}/qm_selected_gridcell_map_tmax_r01_zürich.png"
+model_path = f"{config.SCRATCH_DIR}/temp_r01_HR_masked.nc"
+obs_path = f"{config.SCRATCH_DIR}/TabsD_1971_2023.nc"
+output_path = f"{config.BC_DIR}/qm_temp_r01_singlecell_output.nc"
+plot_path = f"{config.OUTPUTS_MODELS_DIR}/qm_correction_function_temp_r01_geneva.png"
+cdf_plot_path = f"{config.OUTPUTS_MODELS_DIR}/qm_cdf_temp_r01_geneva.png"
+map_plot_path = f"{config.OUTPUTS_MODELS_DIR}/qm_selected_gridcell_map_temp_r01_geneva.png"
 
 print("Loading data")
-model_output = xr.open_dataset(model_path)["tmax"]
-obs_output = xr.open_dataset(obs_path)["TmaxD"]
+model_output = xr.open_dataset(model_path)["temp"]
+obs_output = xr.open_dataset(obs_path)["TabsD"]
 calib_obs = obs_output.sel(time=slice("1981-01-01", "2010-12-31"))
 calib_mod = model_output.sel(time=slice("1981-01-01", "2010-12-31"))
 
@@ -26,6 +26,7 @@ print("lon_vals shape:", lon_vals.shape)
 
 target_lat = 47.3769
 target_lon = 8.5417
+
 dist = np.sqrt((lat_vals - target_lat)**2 + (lon_vals - target_lon)**2)
 i_zurich, j_zurich = np.unravel_index(np.argmin(dist), dist.shape)
 print(f"Closest grid cell to Zurich: i={i_zurich}, j={j_zurich}")
@@ -33,6 +34,7 @@ print(f"Location: lat={lat_vals[i_zurich, j_zurich]}, lon={lon_vals[i_zurich, j_
 
 obs_valid = calib_obs[:, i_zurich, j_zurich].values[~np.isnan(calib_obs[:, i_zurich, j_zurich].values)]
 mod_valid = calib_mod[:, i_zurich, j_zurich].values[~np.isnan(calib_mod[:, i_zurich, j_zurich].values)]
+
 if obs_valid.size == 0 or mod_valid.size == 0:
     print("No valid data for Zurich grid cell. Exiting.")
     exit(1)
@@ -48,32 +50,26 @@ qm_data = np.full(model_output.shape, np.nan, dtype=np.float32)
 qm_data[:, i_zurich, j_zurich] = qm_series.astype(np.float32)
 
 qm_ds = xr.Dataset(
-    {"tmax": (model_output.dims, qm_data)},
+    {"temp": (model_output.dims, qm_data)},
     coords=model_output.coords
 )
 qm_ds.to_netcdf(output_path)
 print(f"Single-cell output saved to {output_path}")
 
 # Correction fx with quantiles : (model - obs)
-quantiles = np.linspace(0, 1, 1001)
+quantiles = np.linspace(0, 1, 99) # 99 quantiles from 0 to 1
 plot_obs_q = np.quantile(obs_valid, quantiles)
 plot_mod_q = np.quantile(mod_valid, quantiles)
 correction = plot_mod_q - plot_obs_q
 lat_val = lat_vals[i_zurich, j_zurich]
 lon_val = lon_vals[i_zurich, j_zurich]
 
-# Setting correction for quantiles >= 0.99 to the value at 0.99
-murdered_correction = correction.copy()
-tail_start_idx = np.where(quantiles >= 0.99)[0][0]
-murdered_correction[tail_start_idx:] = correction[tail_start_idx]
-
 plt.figure(figsize=(7, 5))
-plt.plot(quantiles, correction, label="Original correction", color="blue")
-plt.plot(quantiles, murdered_correction, color="red", linestyle="--", label="Correction with murdered tail at 99 percentile")
+plt.plot(quantiles, correction, label="correction function", color="blue")
 plt.axhline(0, color="gray", linestyle="--", label="No correction")
 plt.xlabel("Quantile")
 plt.ylabel("Correction (Model - Observation) in °C")
-plt.title(f"Correction Function for Daily Max Temperature for \nZürich (lat={lat_val:.3f}, lon={lon_val:.3f})")
+plt.title(f"Correction Function for Daily Average Temperature for \nZürich (lat={lat_val:.3f}, lon={lon_val:.3f})")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
@@ -97,5 +93,3 @@ plt.grid(True)
 plt.tight_layout()
 plt.savefig(cdf_plot_path, dpi=1000)
 print(f"CDF plot saved to {cdf_plot_path}")
-
-print("EQM Zürich validation completed successfully.")
