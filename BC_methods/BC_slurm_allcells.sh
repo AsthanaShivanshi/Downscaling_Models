a65e0fc71ad05b31221c010e901470835b995fcd
sasthana <shivanshi.asthana@unil.ch>
2025-08-01 01:07:47 +0200

    Changes

---
+++ BC_methods/BC_slurm_allcells.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=EQM_Zurich_AllCells
+#SBATCH --output=logs/bc/EQM_zurich_output-%j.txt
+#SBATCH --error=logs/bc/EQM_zurich_job_error-%j.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=256G
+#SBATCH --partition=cpu
+
+module load python
+source environment.sh
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+cd BC_methods
+
+echo "EQM for All Cells started"
+python EQM_allcells.py --n_jobs $SLURM_CPUS_PER_TASK
+echo "EQM for All Cells finished"
