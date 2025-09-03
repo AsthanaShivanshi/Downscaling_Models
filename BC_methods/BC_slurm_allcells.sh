f1410793406f98d480aa2174bd0bda867dbf6cb6
sasthana <shivanshi.asthana@unil.ch>
2025-09-03 16:03:09 +0200

    Changes

--- BC_methods/BC_slurm_allcells.sh
+++ BC_methods/BC_slurm_allcells.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=temp_BC_AllCells
-#SBATCH --output=logs/bc/temp_BC_AllCells_output-%j.txt
-#SBATCH --error=logs/bc/temp_BC_AllCells_job_error-%j.txt
+#SBATCH --job-name=precip_BC_AllCells
+#SBATCH --output=logs/bc/precip_BC_AllCells_output-%j.txt
+#SBATCH --error=logs/bc/precip_BC_AllCells_job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=3-00:00:00
