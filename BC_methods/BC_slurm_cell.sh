f13ef9233bb237f25429277a977c864837e2cd71
sasthana <shivanshi.asthana@unil.ch>
2025-09-10 16:28:13 +0200

    Residual CDFs

--- BC_methods/BC_slurm_cell.sh
+++ BC_methods/BC_slurm_cell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=QDM/BC_DoTC/EQM_cities_TabsD
+#SBATCH --job-name=Residual_DF/QDM/BC_DoTC/EQM_cities_TabsD
 #SBATCH --output=logs/bc/BC_tabs_output-%j.txt
 #SBATCH --error=logs/bc/BC_tabs_job_error-%j.txt
 #SBATCH --ntasks=1
@@ -18,14 +18,18 @@ export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 cd BC_methods
 
-echo "EQM for single cell started"
-python EQM_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
-python EQM_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
+#echo "EQM for single cell started"
+#python EQM_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
+#python EQM_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
 
-echo "DOTC for single cell started"
-python DOTC_BC_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
-python DOTC_BC_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
+#echo "DOTC for single cell started"
+#python DOTC_BC_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
+#python DOTC_BC_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
 
-echo "QDM for single cell started"
-python QDM_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
-python QDM_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
+#echo "QDM for single cell started"
+#python QDM_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
+#python QDM_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
+
+echo "Residual CDF and CvM test"
+python CvM_Bias_of_residuals.py --city Zurich --lat 47.3769 --lon 8.5417
+python CvM_Bias_of_residuals.py --city Locarno --lat 46.1670 --lon 8.7943
