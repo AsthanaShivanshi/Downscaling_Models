3abeff6305a9be23a840ecec12cb01078bf1f5ee
sasthana <shivanshi.asthana@unil.ch>
2025-09-10 13:18:12 +0200

    QDM

--- BC_methods/BC_slurm_cell.sh
+++ BC_methods/BC_slurm_cell.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=BC_DoTC/EQM_cities_TabsD
+#SBATCH --job-name=QDM/BC_DoTC/EQM_cities_TabsD
 #SBATCH --output=logs/bc/BC_tabs_output-%j.txt
 #SBATCH --error=logs/bc/BC_tabs_job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mem=128G
 #SBATCH --partition=cpu
 
@@ -25,3 +25,7 @@ python EQM_single_cell_cities.py --city Locarno --lat 46.1670 --lon 8.7943
 echo "DOTC for single cell started"
 python DOTC_BC_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
 python DOTC_BC_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
+
+echo "QDM for single cell started"
+python QDM_single_cell.py --city Zurich --lat 47.3769 --lon 8.5417
+python QDM_single_cell.py --city Locarno --lat 46.1670 --lon 8.7943
