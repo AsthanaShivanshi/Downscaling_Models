49c9535f7bef63ca9982529044be3b0aab1e78c3
sasthana <shivanshi.asthana@unil.ch>
2025-09-12 13:29:18 +0200

    Changes

--- Training_LDM/models/components/unet.py
+++ Training_LDM/models/components/unet.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+from lightning import LightningModule
+
+#Additional citation : https://github.com/soof-golan/spacecutter-lightning/tree/14febad15c47ba1861c7c3c3397a1929fd47e65d/README.md
 
 #AsthanaSh : This is the reference Unet upon which the GAN is based.
 class DoubleConv(nn.Module):
@@ -48,41 +51,54 @@ class DecoderBlock(nn.Module):
 class DownscalingUnet(nn.Module):
     def __init__(self, in_ch=1, out_ch=1, features=[64,128,256,512]):
         super().__init__()
-        self.encoder = nn.ModuleList()
-        self.decoder = nn.ModuleList()
-        """ Encoder """
         self.e1 = EncoderBlock(in_ch, features[0])
         self.e2 = EncoderBlock(features[0], features[1])
         self.e3 = EncoderBlock(features[1], features[2])
         self.e4 = EncoderBlock(features[2], features[3])         
-        """ Bottleneck """
         self.b = DoubleConv(features[3], features[3]*2)         
-        """ Decoder """
         self.d1 = DecoderBlock(features[3]*2, features[3])
         self.d2 = DecoderBlock(features[3], features[2])
         self.d3 = DecoderBlock(features[2], features[1])
         self.d4 = DecoderBlock(features[1], features[0])         
-        """ Classifier """
         self.outputs = nn.Conv2d(features[0], out_ch, kernel_size=1, padding=0)
-    
-    def forward(self, inputs):
-        """ Encoder """
-        # note: ss are for the skip connections!
-        #       ps are results of an encoder block!
-        s1, p1 = self.e1(inputs)
+
+    def forward(self, x):
+        # Encoder
+        s1, p1 = self.e1(x)
         s2, p2 = self.e2(p1)
         s3, p3 = self.e3(p2)
         s4, p4 = self.e4(p3)
-        """ Bottleneck """
+        # Bottleneck
         b = self.b(p4)
-        """ Decoder """
+        # Decoder
         d1 = self.d1(b, s4)
         d2 = self.d2(d1, s3)
         d3 = self.d3(d2, s2)
         d4 = self.d4(d3, s1)
-        """ Classifier """
-        outputs = self.outputs(d4)
-        return outputs
+        # Output
+        out = self.outputs(d4)
+        return out
+
+
+
+class DownscalingUnetLightning(LightningModule):
+    def __init__(self, in_ch=1, out_ch=1, features=[64,128,256,512]):
+        super().__init__()
+        self.unet = DownscalingUnet(in_ch, out_ch, features)
+        self.loss_fn = nn.MSELoss() 
+
+    def forward(self, x):
+        return self.unet(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss_fn(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
     
     def last_layer(self):
-        return self.outputs
+        return self.unet.outputs
