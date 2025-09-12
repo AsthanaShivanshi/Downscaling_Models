a87370efc949f12ae7a32094c62556043701ada7
sasthana <shivanshi.asthana@unil.ch>
2025-09-12 11:18:48 +0200

    Changes to project paths 8discrete)

--- Training_LDM/trainer.sh
+++ Training_LDM/trainer.sh
@@ -9,31 +9,22 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
+source diffscaler.sh
+
 mkdir -p logs
 
-echo "Loading python module"
-module load python
-
-module load micromamba
-module load gcc/11.2.0
-
-echo "Sourcing environment"
-
-eval "$(micromamba shell hook -s bash)"
-micromamba activate /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling_Models/.micromamba/envs/diffscaler
-
-cd Training_LDM
+cd "$PROJECT_DIR"
 
 export WANDB_MODE=online
 export PYTHONUNBUFFERED=1
 which python
 python --version
 
-#Training the Unet
-python train.py --config-name UNet_config.yaml
+# Training the Unet
+python Training_LDM/train.py --config-name UNet_config.yaml
 
-#Training the VAE
-#python train.py --config-name VAE_config.yaml   
+# Training the VAE
+#python Training_LDM/train.py --config-name VAE_config.yaml   
 
-#Training the LDM
-#python train.py --config-name LDM_config.yaml
+# Training the LDM
+#python Training_LDM/train.py --config-name LDM_config.yaml
