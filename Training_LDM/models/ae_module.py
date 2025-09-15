5eb864a558085e62211af83582f8a48548c96b21
sasthana <shivanshi.asthana@unil.ch>
2025-09-15 17:37:18 +0200

    Changes

--- Training_LDM/models/ae_module.py
+++ Training_LDM/models/ae_module.py
@@ -117,14 +117,12 @@ class AutoencoderKL(LightningModule):
         }
     
     def preprocess_batch(self, batch):
-        low_res, high_res, smt = batch  # All at high-res (1km)
+        low_res, high_res = batch  # static is already in low_res, concatenated according to DataModule code 
         if self.ae_flag is None:
             return low_res, high_res
         elif self.ae_flag == 'residual':
-            #  static to low_res concatenation
-            low_res_with_static = torch.cat([low_res, smt], dim=1)  # [B, 5, H, W]
-            # UNet predicts high-res from bicubic+static
-            unet_pred = self.unet_regr(low_res_with_static)
+            # UNet expects all 5 channels (static included)
+            unet_pred = self.unet_regr(low_res)
             residual = high_res - unet_pred
             return residual, residual
         elif self.ae_flag == 'hres':
