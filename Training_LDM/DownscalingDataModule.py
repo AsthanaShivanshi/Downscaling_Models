8ffd5bbde996153397f34f2429d355ce15bb8fce
sasthana <shivanshi.asthana@unil.ch>
2025-09-29 10:54:02 +0200

    Changes

--- Training_LDM/DownscalingDataModule.py
+++ Training_LDM/DownscalingDataModule.py
@@ -31,6 +31,10 @@ class DownscalingDataModule(LightningDataModule):
         self.num_workers = num_workers
         self.preprocessing = preprocessing or {}
 
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
+
     def setup(self, stage=None):
         # Load elevation once as array
         if self.elevation is not None and isinstance(self.elevation, str):
@@ -40,37 +44,41 @@ class DownscalingDataModule(LightningDataModule):
         else:
             elevation_array = self.elevation
 
-        train_input_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.train_input.items()}
-        train_target_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.train_target.items()}
-        self.train_dataset = DownscalingDataset(
-            input_ds=train_input_ds,
-            target_ds=train_target_ds,
-            config={"variables": self.preprocessing.get("variables", {}),
-                    "preprocessing": self.preprocessing.get("preprocessing", {})},
-            elevation_path=elevation_array,
-        )
-        # Val
-        if self.val_input and self.val_target:
-            val_input_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.val_input.items()}
-            val_target_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.val_target.items()}
-            self.val_dataset = DownscalingDataset(
-                input_ds=val_input_ds,
-                target_ds=val_target_ds,
-                config={"variables": self.preprocessing.get("variables", {}),
-                        "preprocessing": self.preprocessing.get("preprocessing", {})},
+        if (
+            isinstance(self.train_input, dict) and
+            isinstance(self.train_target, dict) and
+            len(self.train_input) > 0 and
+            len(self.train_target) > 0
+        ):
+            train_input_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.train_input.items()}
+            train_target_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.train_target.items()}
+            self.train_dataset = DownscalingDataset(
+                input_ds=train_input_ds,
+                target_ds=train_target_ds,
+                config={
+                    "variables": self.preprocessing.get("variables", {}),
+                    "preprocessing": self.preprocessing.get("preprocessing", {})
+                },
                 elevation_path=elevation_array,
             )
         else:
-            self.val_dataset = None
-        # Test
-        if self.test_input and self.test_target:
+            self.train_dataset = None
+
+        if (
+            isinstance(self.test_input, dict) and
+            isinstance(self.test_target, dict) and
+            len(self.test_input) > 0 and
+            len(self.test_target) > 0
+        ):
             test_input_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.test_input.items()}
             test_target_ds = {k: xr.open_dataset(v, engine='netcdf4') for k, v in self.test_target.items()}
             self.test_dataset = DownscalingDataset(
                 input_ds=test_input_ds,
                 target_ds=test_target_ds,
-                config={"variables": self.preprocessing.get("variables", {}),
-                        "preprocessing": self.preprocessing.get("preprocessing", {})},
+                config={
+                    "variables": self.preprocessing.get("variables", {}),
+                    "preprocessing": self.preprocessing.get("preprocessing", {})
+                },
                 elevation_path=elevation_array,
             )
         else:
@@ -78,7 +86,9 @@ class DownscalingDataModule(LightningDataModule):
 
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+        if self.train_dataset:
+            return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True)
+        return None
 
     def val_dataloader(self):
         if self.val_dataset:
