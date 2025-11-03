78689ae0d7a9399b6de735d7659d5fe9a39c89aa
sasthana <shivanshi.asthana@unil.ch>
2025-11-03 20:47:44 +0100

    Update submodule

--- BC_methods/dOTC_BC_AllCells.py
+++ BC_methods/dOTC_BC_AllCells.py
@@ -10,6 +10,8 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 var_names = ["temp", "precip", "tmin", "tmax"]
 obs_var_names = ["TabsD", "RhiresD", "TminD", "TmaxD"]
 
+
+
 model_paths = [
     f"{config.MODELS_DIR}/temp_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_rcp85_1971-2099/temp_r01_coarse_masked.nc",
     f"{config.MODELS_DIR}/precip_MPI-CSC-REMO2009_MPI-M-MPI-ESM-LR_rcp85_1971-2099/precip_r01_coarse_masked.nc",
@@ -24,9 +26,12 @@ obs_paths = [
     f"{config.DATASETS_TRAINING_DIR}/TmaxD_step2_coarse.nc"
 ]
 
-print("Loading datasets...")
+
 model_datasets = [xr.open_dataset(p)[vn] for p, vn in zip(model_paths, var_names)]
 obs_datasets = [xr.open_dataset(p)[ovn] for p, ovn in zip(obs_paths, obs_var_names)]
+
+
+
 
 ntime, nN, nE = model_datasets[0].shape
 nvars = len(var_names)
@@ -35,13 +40,28 @@ print(f"Data shape: {ntime} time steps, {nN}x{nE} grid cells, {nvars} variables"
 model_times = model_datasets[0]['time'].values
 obs_times = obs_datasets[0]['time'].values
 
+
+
+
+
+
+
+
+
 def get_doy(d): 
     return (np.datetime64(d, 'D') - np.datetime64(str(d)[:4] + '-01-01', 'D')).astype(int) + 1
 
 model_doys = np.array([get_doy(d) for d in model_times])
 
+
+
+
 calib_start = np.datetime64("1981-01-01")
 calib_end = np.datetime64("2010-12-31")
+
+
+
+
 
 def process_cell(i, j):
     full_mod_cells = [ds[:, i, j].values for ds in model_datasets]  # Full period
@@ -65,6 +85,9 @@ def process_cell(i, j):
     calib_obs_stack = np.stack(calib_obs_cells, axis=1)
     calib_doys = full_doys[calib_model_mask]
 
+
+
+
     if "precip" in var_names:
         precip_idx = var_names.index("precip")
         calib_obs_stack[:, precip_idx] = np.clip(calib_obs_stack[:, precip_idx], 0, None)
@@ -72,6 +95,9 @@ def process_cell(i, j):
         full_mod_stack[:, precip_idx] = np.clip(full_mod_stack[:, precip_idx], 0, None)
 
     full_corrected_stack = np.full_like(full_mod_stack, np.nan)
+
+
+
 
     for doy in range(1, 367):
         window_diffs = (calib_doys - doy + 366) % 366
@@ -120,7 +146,7 @@ def process_cell(i, j):
                 mean_diff = np.nanmean(calib_obs_win_clean, axis=0) - np.nanmean(calib_mod_win_clean, axis=0)
                 corrected_full = full_mod_win_clean + mean_diff
 
-        if "precip" in var_names:
+        if "precip" in var_names:  #Debug step
             precip_idx = var_names.index("precip")
             corrected_full[:, precip_idx] = np.clip(corrected_full[:, precip_idx], 0, None)
 
@@ -132,7 +158,9 @@ def process_cell(i, j):
 
     return full_corrected_stack
 
-print("initiating bias corr using dotc...")
+print("initiating bc dotc")
+
+
 cell_pairs = [(i, j) for i in range(nN) for j in range(nE)]
 results = Parallel(n_jobs=8)(
     delayed(process_cell)(i, j)
@@ -140,7 +168,7 @@ results = Parallel(n_jobs=8)(
 )
 
 
-print("Recon...")
+print("Recon")
 corrected_data = {var: np.full((ntime, nN, nE), np.nan, dtype=np.float32) for var in var_names}
 
 idx = 0
@@ -152,7 +180,8 @@ for i in range(nN):
                 corrected_data[var][:, i, j] = cell_result[:, v]
         idx += 1
 
-print("Saving each varfile separately...")
+
+
 for v, var_name in enumerate(var_names):
     original_model_ds = xr.open_dataset(model_paths[v])
     
@@ -174,11 +203,10 @@ for v, var_name in enumerate(var_names):
     # Close the dataset to free memory
     original_model_ds.close()
 
-print("\nDiagnostics:")
+print("diagnostics of valid points")
 for var in var_names:
     non_nan_count = np.sum(~np.isnan(corrected_data[var]))
     total_count = corrected_data[var].size
     percentage = 100 * non_nan_count / total_count
     print(f"{var}: {non_nan_count}/{total_count} non-NaN values ({percentage:.1f}%)")
 
-print("dOTC done")
