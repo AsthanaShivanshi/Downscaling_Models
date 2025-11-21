59fb9131c094a65e47e1ffea18af29182c957012
sasthana <shivanshi.asthana@unil.ch>
2025-11-21 18:19:18 +0100

    Changes

--- UNet_Deterministic_Training_Dataset/UNet_Experiments.sh
+++ UNet_Deterministic_Training_Dataset/UNet_Experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=Unconstrained_Channels_UNet_Optuna_Training_Dataset_Optimisation
-#SBATCH --output=logs/training_unconstrained/job_output-%j.txt
-#SBATCH --error=logs/training_unconstrained/job_error-%j.txt
+#SBATCH --job-name=UNet_Deterministic_Training_Dataset
+#SBATCH --output=logs/training_UNet/job_output-%j.txt
+#SBATCH --error=logs/training_UNet/job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=3-00:00:00
@@ -24,9 +24,9 @@ export WANDB_MODE=online
 export PYTHONUNBUFFERED=1
 which python
 python --version
-echo "Starting Optuna Optimisation..."
-#python #Main.py --quick_test
+#echo "Starting Optuna Optimisation..."
+python Main.py #--quick_test
 
 #For full training, remove the --quick_test flag, with 1000 samples, a smoke test
-python optuna_optimisation_unconstrained.py         #Main.py
-echo "Optuna optimisation finished."
+#python optuna_optimisation_unconstrained.py         #Main.py
+#echo "Optuna optimisation finished."
--- UNet_Deterministic_Training_Dataset/run_inference.sh
+++ UNet_Deterministic_Training_Dataset/run_inference.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 
 mkdir -p logs/training
 module load python
