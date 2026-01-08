21021e67057b2d618d41f8184f9c54000005a21b
sasthana <shivanshi.asthana@unil.ch>
2026-01-08 13:23:50 +0100

    Changes

--- LDM_conditional/train.py
+++ LDM_conditional/train.py
@@ -36,10 +36,10 @@ def train(cfg: DictConfig):
         unet_model.load_state_dict(new_state_dict)
         print("Loaded UNet mean regression model from:", cfg.model.unet_regr)
 
-    # Instantiate model: for UNet and VAE
+    # Instantiate model: for UNet and VAE,,,,for Unet, do NOT forget to pass the RhiresD params for forward pass changes (see source code): TO SELF!!!!!!!
     model: LightningModule = hydra.utils.instantiate(cfg.model, unet_regr=unet_model if cfg.model.get("unet_regr") else None)
 
-    #Checking that files exist /debugging prints cz val files were not being read: AsthanaSh
+    # val files were not being read: AsthanaSh
     print("DataModule val files", datamodule.val_input, datamodule.val_target)
     if isinstance(datamodule.val_input, dict):
         for k, v in datamodule.val_input.items():
@@ -60,7 +60,7 @@ def train(cfg: DictConfig):
     wandb.finish()  # Close any previous run
 
     run_name = f"run_{cfg.get('hydra', {}).get('job', {}).get('id', os.getpid())}" #unique for each run,,, appends job id
-    logger = WandbLogger(project="UNet_optim_run_12km_bivariate", log_model=True, name=run_name)
+    logger = WandbLogger(project="VAE_optim_run_12km_bivariate", log_model=True, name=run_name)
     logger.experiment.config.update({
         #"kl_weight": cfg.model.get("kl_weight"),
         #"latent_dim": cfg.model.get("latent_dim"),
@@ -99,7 +99,7 @@ def train(cfg: DictConfig):
     ckpt_path = trainer.checkpoint_callback.best_model_path if hasattr(trainer, "checkpoint_callback") else None
     trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
 
-@hydra.main(version_base="1.3", config_path="configs", config_name="UNet_bivariate_config.yaml")
+@hydra.main(version_base="1.3", config_path="configs", config_name="VAE_bivariate_config.yaml")
 def main(cfg: DictConfig):
     train(cfg)
 
