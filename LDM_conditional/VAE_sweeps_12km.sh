e15d158d04667bc4ba24e0f95aa645c7f646f92d
sasthana <shivanshi.asthana@unil.ch>
2026-01-16 15:15:08 +0100

    Changes

--- LDM_conditional/VAE_sweeps_12km.sh
+++ LDM_conditional/VAE_sweeps_12km.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=logs/ckpts_LDM/VAE/Log_VAEMAESweep_job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=12:00:00
+#SBATCH --time=8:00:00
 #SBATCH --mem=128G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -24,12 +24,12 @@ python -c "import wandb; print(wandb.__version__)"
 
 #vae sweep :mae sweep
 
-for latent_dim in 32 64 128; do
+for latent_dim in 8 16 24 60 72 90; do
   for kl_weight in 0.001 0.01 0.1; do
     sbatch --job-name=VAE_ld${latent_dim}_kl${kl_weight} \
       --output=logs/ckpts_LDM/VAE/vae_ld${latent_dim}_kl${kl_weight}_%j.out \
       --error=logs/ckpts_LDM/VAE/vae_ld${latent_dim}_kl${kl_weight}_%j.err \
-      --ntasks=1 --cpus-per-task=4 --time=12:00:00 --mem=128G --partition=gpu --gres=gpu:1 \
+      --ntasks=1 --cpus-per-task=4 --time=8:00:00 --mem=128G --partition=gpu --gres=gpu:1 \
       --wrap="source ../diffscaler.sh && export PYTHONPATH='$PROJECT_DIR' && cd '$PROJECT_DIR' && \
         export WANDB_MODE=online && export WANDB_START_METHOD=thread && export PYTHONUNBUFFERED=1 && export HYDRA_FULL_ERROR=1 && \
         python LDM_conditional/train.py --config-name VAE_bivariate_config.yaml vae.latent_dim=${latent_dim} vae.kl_weight=${kl_weight}"
