5240548a5f7e8bcf914eac6f0c076f709d40b055
sasthana <shivanshi.asthana@unil.ch>
2026-01-30 13:57:27 +0100

    Changes

--- DDIM_conditional_derived/inference.sh
+++ DDIM_conditional_derived/inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=AllFrames_DDIM_inference
-#SBATCH --output=DDIM_conditional_derived/logs/ckpts_DDIM/AllFrames_DDIM_inference-job_output-%j.txt
-#SBATCH --error=DDIM_conditional_derived/logs/ckpts_DDIM/AllFrames_DDIM_inference-job_error-%j.txt
+#SBATCH --output=DDIM_conditional_derived/logs/ckpts_DDIM/2samples_AllFrames_DDIM_inference-job_output-%j.txt
+#SBATCH --error=DDIM_conditional_derived/logs/ckpts_DDIM/2samples_AllFrames_DDIM_inference-job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=3-00:00:00
@@ -20,10 +20,9 @@ export HYDRA_FULL_ERROR=1
 which python
 python -c "import wandb; print(wandb.__version__)"
 
-#python DDIM_conditional_derived/inference_single_frame_hierarchy.py --idx 12 --sampling_steps 500
-#python DDIM_conditional_derived/inference_single_frame_hierarchy.py --idx 12 --sampling_steps 250
-#python DDIM_conditional_derived/inference_single_frame_hierarchy.py --idx 12 --sampling_steps 999
+#python DDIM_conditional_derived/inference_single_frame_hierarchy.py --idx 12 --sampling_steps 250 500 750 999
+#python DDIM_conditional_derived/inference_single_frame_hierarchy.py --idx 25 --sampling_steps 250 500 750 999
+#python DDIM_conditional_derived/inference_single_frame_hierarchy.py --idx 31 --sampling_steps 250 500 750 999
 
-
-
+#Single run determinisitc DDIm inference ... all frames. 
 python DDIM_conditional_derived/inference_allframes_eta0.py
