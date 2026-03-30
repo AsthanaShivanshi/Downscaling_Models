9cc139126a965f6a96d3d7cdaf9e1c5d2d580df0
sasthana <shivanshi.asthana@unil.ch>
2026-03-30 15:54:24 +0200

    Changes

--- LDM_conditional/trainer_sweeps_12km_UNet_MAE.sh
+++ LDM_conditional/trainer_sweeps_12km_UNet_MAE.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-##EOF: heredoc for passing block to sbatch instead of creating separate scripts. 
 
-for config in UNet_bivariate_config_12km.yaml; do
+config="UNet_bivariate_config_12km.yaml"
+
+for weight in 1.0 3.0 5.0 7.0; do
   sbatch <<EOF
 #!/bin/bash
-#SBATCH --job-name=${config%.yaml}_Log_MAE_UNet_MAE_Sweep
-#SBATCH --output=logs/ckpts_LDM/UNet/${config%.yaml}_Log_UNetMAESweep_job_output-%j.txt
-#SBATCH --error=logs/ckpts_LDM/UNet/${config%.yaml}_Log_UNetMAESweep_job_error-%j.txt
+#SBATCH --job-name=${config%.yaml}_Log_MAE_UNet_MAE_Sweep_${weight}
+#SBATCH --output=logs/ckpts_LDM/UNet/${config%.yaml}_Log_UNetMAESweep_${weight}_job_output-%j.txt
+#SBATCH --error=logs/ckpts_LDM/UNet/${config%.yaml}_Log_UNetMAESweep_${weight}_job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=15:00:00
@@ -16,7 +17,7 @@ for config in UNet_bivariate_config_12km.yaml; do
 #SBATCH --gres=gpu:1
 
 source ../diffscaler.sh
-mkdir -p logs/ckpts_LDM/UNet/${config%.yaml}
+mkdir -p logs/ckpts_LDM/UNet/
 
 cd "/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling_Models"
 export PYTHONPATH="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling_Models"
@@ -31,9 +32,8 @@ python -c "import wandb; print(wandb.__version__)"
 
 #unet sweep :mae sweep
 
-python LDM_conditional/train.py --multirun --config-name ${config} \
-  model.precip_loss_weight=1.0,3.0,5.0,7.0 \
-  model.use_crps_channels="[0,1]" \
+python LDM_conditional/train.py --config-name UNet_bivariate_config_12km.yaml \
+  model.precip_loss_weight=${weight} \
   model.lr=0.001
 EOF
 done
