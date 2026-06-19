e44e2754b8be317b025786c94ca33739f4606398
sasthana <shivanshi.asthana@unil.ch>
2026-06-19 18:49:47 +0200

    Changes

--- environment.sh
+++ environment.sh
@@ -4,10 +4,10 @@
 export BASE_DIR="/work/FAC/FGSE/IDYST/tbeucler/downscaling"
 export ENVIRONMENT="${BASE_DIR}/sasthana/MyPythonEnvNew"
 
-module load micromamba
-eval "$(micromamba shell hook --shell=bash)"
+module load mamba
+eval "$(mamba shell hook --shell=bash)"
 export PROJ_LIB="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/MyPythonEnvNew/share/proj"
-micromamba activate "$ENVIRONMENT"
+mamba activate "$ENVIRONMENT"
 
 echo $PROJ_LIB
 cd "$BASE_DIR/sasthana/Downscaling/Downscaling_Models/"
